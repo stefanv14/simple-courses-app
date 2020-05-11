27968eccc31cebaa098784ae48e677b91cdedc67
stefanv14 <vasilijevic032@gmail.com>
2020-05-11 18:21:25 +0200

    Updates

---
+++ precache-manifest.2a807153e147700c7644330ae56620d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8171d0c98692392ccb0abfeb3899f6",
+    "url": "/simple-courses-app/index.html"
+  },
+  {
+    "revision": "eb08b325cca0f70a92b6",
+    "url": "/simple-courses-app/static/css/main.9f8122a6.chunk.css"
+  },
+  {
+    "revision": "e1b27ad05f14a3be671e",
+    "url": "/simple-courses-app/static/js/2.f195eae5.chunk.js"
+  },
+  {
+    "revision": "eb08b325cca0f70a92b6",
+    "url": "/simple-courses-app/static/js/main.a7144ba8.chunk.js"
+  },
+  {
+    "revision": "50186540b653bfd88c2e",
+    "url": "/simple-courses-app/static/js/runtime-main.451a57cc.js"
+  },
+  {
+    "revision": "66e73c75cc64d06cb82c21fe82a8413c",
+    "url": "/simple-courses-app/static/media/appBackground2.66e73c75.svg"
+  },
+  {
+    "revision": "483f31360b8c5c5df36e04f06780b3c7",
+    "url": "/simple-courses-app/static/media/logo.483f3136.svg"
+  }
+]);
